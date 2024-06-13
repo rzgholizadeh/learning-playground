3202c76ff524f78e4e76b83035e913e3072fcbc8
Reza <rzgholizadeh@gmail.com>
2024-06-13 16:59:48 -0700

    Update event-bus to resolve the middleware bug

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -3,13 +3,20 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
-app.post('/event', (req, res) => {
+app.post('/events', async (req, res) => {
+    console.log('We got the event');
     const event = req.body;
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    await axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    await axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    await axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
 
     res.send({ status: 'OK' });
 });
