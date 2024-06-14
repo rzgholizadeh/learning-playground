912df6fe69cccdd954f0e0981bdcc93357a2bb6b
Reza <rzgholizadeh@gmail.com>
2024-06-13 17:00:11 -0700

    Add new route

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,18 +10,30 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req,res)=>{
+app.get('/posts', (req, res) => {
     res.send(posts);
 });
-app.post('/posts', (req,res)=>{
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
-    const {title} = req.body;
-    posts[id]={
+    const { title } = req.body;
+    posts[id] = {
         id,
         title
     };
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    }).catch((err) => {
+        console.log(err.message);
+    });
     res.status(201).send(posts[id]);
 });
-app.listen(4000, ()=>{
-   console.log('Post App is listening on 4000...'); 
+app.post('/events', (req, res) => {
+    console.log('Received event:', req.body.type);
+    res.send({});
+});
+app.listen(4000, () => {
+    console.log('Post App is listening on 4000...');
 });
