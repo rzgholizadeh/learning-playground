54ab2b03b7de83b3c91ca247eac067bbd58e1954
Reza <rzgholizadeh@gmail.com>
2024-06-16 00:23:34 -0700

    Add comment moderation and status presentation feature

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -4,9 +4,22 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
-app.post('/events', (req, res) => {
-    console.log('Received event:', req.body.type);
-
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                content: data.content,
+                postId: data.postId,
+                status
+            }
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    }
     res.send({});
 });
 app.listen(4003, () => {
