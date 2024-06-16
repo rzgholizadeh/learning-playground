0982af2ce2f04c19b61884668f71eddbae060419
Reza <rzgholizadeh@gmail.com>
2024-06-15 23:45:33 -0700

    Add boilerplate code for moderation service

---
+++ blog/moderation/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+app.post('/events', (req, res) => {
+    console.log('Received event:', req.body.type);
+
+    res.send({});
+});
+app.listen(4003, () => {
+    console.log('Moderation app listening on port 4003...');
+});
