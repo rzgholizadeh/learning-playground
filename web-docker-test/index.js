02785146c35c7700785be5a378fd85f95e588b49
Reza <rzgholizadeh@gmail.com>
2024-06-20 19:31:41 -0700

    Change the message

--- web-docker-test/index.js
+++ web-docker-test/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('fuck you too');
+    res.send('fuck you too, as well');
 });
 
 app.listen(8080, () => {
