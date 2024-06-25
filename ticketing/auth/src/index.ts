3b9101c84fd26ba6beea615176cef9dff49929b1
Reza <rzgholizadeh@gmail.com>
2024-06-24 23:14:33 -0700

    Use routers

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signUpRouter } from './routes/signup';
+import { signOutRouter } from './routes/signout';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signUpRouter);
+app.use(signOutRouter);
+
 
 app.listen(3000, () => {
     console.log('auth app listening on port 3000');
