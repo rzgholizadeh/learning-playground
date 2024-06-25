b7f70765b392d2debd29cbdaeb8e2b2a2f3e0f3b
Reza <rzgholizadeh@gmail.com>
2024-06-24 23:22:49 -0700

    Use validator package for email and password

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.get('/api/users/signup', (req, res) => {
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password must be min 4 and max 20 characters')
+], (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
     res.send('Hello world');
 });
 
