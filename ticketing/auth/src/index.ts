09e2b508a918699037824fd6982a54feae49d3bb
Reza <rzgholizadeh@gmail.com>
2024-06-24 23:12:05 -0700

    Add boilerplate code for auth routers

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 app.use(json());
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi world!');
-});
+
+app.use(currentUserRouter);
+
 app.listen(3000, () => {
     console.log('auth app listening on port 3000');
 });
