02e6c6ff4ef4cead607eb32c81825fb3dca662d1
Reza <rzgholizadeh@gmail.com>
2024-06-24 23:28:34 -0700

    Send error states of validation to client

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,20 +1,28 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be min 4 and max 20 characters')
-], (req: Request, res: Response) => {
-    const { email, password } = req.body;
-
-    res.send('Hello world');
-});
+router.post(
+    '/api/users/signup',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Password must be min 4 and max 20 characters')
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send(errors.array());
+        }
+        const { email, password } = req.body;
+        console.log('Creating a user...');
+        res.send({});
+    }
+);
 
 export { router as signUpRouter }
