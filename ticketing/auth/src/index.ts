9d9b07b31f44db5033512c8446ef4f3ef0c886a2
Reza <rzgholizadeh@gmail.com>
2024-06-25 22:00:42 -0700

    Add error-handler middleware

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signUpRouter } from './routes/signup';
 import { signOutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -13,6 +14,7 @@ app.use(signInRouter);
 app.use(signUpRouter);
 app.use(signOutRouter);
 
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('auth app listening on port 3000');
