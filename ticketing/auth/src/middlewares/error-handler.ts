e519e3291b3ee43f3447da9d45ee88de34c6f29d
Reza <rzgholizadeh@gmail.com>
2024-06-26 21:25:08 -0700

    Serialize error messages and add statuscode

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -8,19 +8,10 @@ export const errorHandler = (
     res: Response,
     next: NextFunction) => {
     if (err instanceof RequestValidationError) {
-        const formattedErrors = err.errors.map((error) => {
-            if (error.type === 'field') {
-                return { message: error.msg, field: error.path };
-            }
-        });
-        return res.status(400).send({ errors: formattedErrors });
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
     if (err instanceof DatabaseConnectionError) {
-        return res.status(500).send({
-            errors: [
-                { message: err.reason }
-            ]
-        });
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
     res.status(400).send({
         errors: [{ message: 'Something went wrong' }]
