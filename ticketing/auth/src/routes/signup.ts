396085cb1f8e15c416b414eb07d2b9b78c2932be
Reza <rzgholizadeh@gmail.com>
2024-07-08 19:48:40 -0700

    Check for already existing emails

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connenction-error';
+import { User } from '../models/user';
+
 
 const router = express.Router();
 
@@ -16,14 +17,17 @@ router.post(
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be min 4 and max 20 characters')
     ],
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array());
         }
-        const { email, password } = req.body;
-        console.log('Creating a user...');
-        res.send({});
+        const { email } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            console.log('Email already in use');
+            return res.send({});
+        }
     }
 );
 
